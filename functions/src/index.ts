c9390d67b076768f53a2371b3fdd87ff6c111b3a
Daniel Suchan <mr.sucik@gmail.com>
2021-10-30 11:14:41 +0200

    Create scheduled and manual game update Cloud Functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,31 @@
 import * as functions from "firebase-functions";
+import { doc } from "./fire";
+import Grid from "./game/grid";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+type GenerationDocument = {
+  data: string;
+};
+
+type GenerationParsed = boolean[][];
+
+const updateGame = async () => {
+  const docData = await doc.get();
+  const { data } = docData.data() as GenerationDocument;
+  const parsed = JSON.parse(data) as GenerationParsed;
+  const grid = new Grid(parsed);
+
+  grid.nextIteration();
+
+  const nextData = grid.exportAsDefinition();
+  doc.update({ data: JSON.stringify(nextData) });
+};
+
+export const scheduledUpdate = functions.pubsub
+  .schedule("every 1 second")
+  .onRun(async () => {
+    await updateGame();
+  });
+
+export const manualUpdate = functions.https.onRequest(async () => {
+  await updateGame();
+});
